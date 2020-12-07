c1a298569f8e07415aec08bc12647b659abcd3a3
iths-linus-kallin <jaglinus@gmail.com>
2020-12-07 15:01:40 +0100

    front-page edited, page-type 'hamnar' created

--- functions.php
+++ functions.php
@@ -56,4 +56,41 @@
 
   add_action('widgets_init', 'aoh_event_widget_area');
 
+  function aoh_create_post_type_dock(){
+    register_post_type('Hamn', 
+      array(
+        'labels' => array(
+          'name' => __('Hamnar'),
+          'singular_name' => __('Hamn')
+        ),
+        'supports' => array(
+          'title',
+          'editor',
+          'excerpt',
+          'author',
+          'thumbnail',
+          'comments',
+          'revisions',
+          'custom-fields'
+        ),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 1,
+        'menu_icon' => 'dashicons-palmtree',
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'capability_type' => 'page',
+        'show_in_rest' => true
+      )
+    );
+  }
+
+  add_action('init', 'aoh_create_post_type_dock');
+
 ?>
