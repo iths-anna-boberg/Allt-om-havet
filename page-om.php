47563924d6a1284d7abfc16ec2332852a6811084
iths-jonathan-engman <54362354+iths-jonathan-engman@users.noreply.github.com>
2020-12-08 11:44:52 +0100

    base content

--- page-om.php
+++ page-om.php
@@ -1,8 +1,23 @@
-<?php
+<?php get_header(); ?>
 
-  get_header();
+ <div class="container wrapper">
 
-?>
+    <h1>Om oss</h1>
 
-<?php
-get_footer();
+    <div class="row"></div>
+        <div class="col-6">
+            <p> Allt Om Havet innehåller uppgifter om samtliga gästhamnar som erbjuder platser för besökande, i dagsläget cirka 590 hamnar.
+                Här kan Du som användare finna de uppgifter som kan vara av intresse innan ni besöker hamnen. Bland annat kan Ni finna om det finns bränsle, livsmedel, läkare, bank, dusch eller systembolag i aktuell hamn.
+                Totalt finns det över 30 viktiga uppgifter att finna på varje hamn. Sjöräddningssällskapets samtliga räddningsstationer finns angivna för de områden där de är verksamma, något som i vissa situationer kan vara direkt livsavgörande.
+                Vi har även sammanställt uppgifter om sevärdheter i och omkring hamnarna. Dessa uppgifter samlar vi in årligen från hamncheferna, kommunerna samt de lokala turistbyråerna.
+                </p>
+        </div>
+        <div class="col-6">
+        <img src="/mockup-assets/windows_crew.png" width="280" height="125" title="" alt="" />
+        </div>
+
+    </div>
+
+ </div>
+
+<?php get_footer(); ?>
