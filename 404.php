db773fa19eeaec66d3b51ada60c9588b09798bff
David Larsson <davvelars@gmail.com>
2020-12-08 10:58:28 +0100

    404 content klar

--- 404.php
+++ 404.php
@@ -3,23 +3,29 @@
  * The template for displaying 404 pages (Not Found)
  */
 
-get_header();
+get_header('small', false);
 ?>
 
 <section class="container col">
   <article class="col">
-  <h1 class="row justify-content-center">
-    Not found
-  </h1>
-  <div class="row justify-content-center">
-  <?php 
-    $img = wp_get_attachment_image( 9, 'medium' );
-    echo $img;
-  ?>
-  </div>
-  <div class="row justify-content-center">
-  <a href="/"><button class="aoh-btn-solid">Gå till startsida</button></a>
-  </div>
+    <h1 class="row justify-content-center">
+      Not found
+    </h1>
+    <div class="row justify-content-center p-5">
+      <?php 
+        $img = wp_get_attachment_image( 9, 'medium' );
+        echo $img;
+      ?>
+    </div>
+    <div class="row justify-content-center">
+      <p class="text-center w-50">Hoppsan hejsan! Vi kunde inte hitta sidan du letade efter.
+        Det beror förmodligen på att den inte finns.
+        Testa igen eller gå tillbaka till startsidan.
+      </p>
+    </div>
+    <div class="row justify-content-center">
+      <a href="/"><button class="aoh-btn-solid">Gå till startsida</button></a>
+    </div>
   </article>
 </section>
 
