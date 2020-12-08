86f36fdad73a81973e40d0cfd4ec3d845234b17d
David Larsson <davvelars@gmail.com>
2020-12-08 12:23:37 +0100

    responsivitetsfix

--- 404.php
+++ 404.php
@@ -7,24 +7,26 @@ get_header('small', false);
 ?>
 
 <section class="container col">
-  <article class="col">
-    <h1 class="row justify-content-center">
+  <article class="col aoh-404-content">
+    <h1 class="row justify-content-center border-bottom border-dark">
       Not found
     </h1>
-    <div class="row justify-content-center p-5">
-      <?php 
-        $img = wp_get_attachment_image( 9, 'medium' );
-        echo $img;
-      ?>
-    </div>
-    <div class="row justify-content-center">
-      <p class="text-center w-50">Hoppsan hejsan! Vi kunde inte hitta sidan du letade efter.
-        Det beror förmodligen på att den inte finns.
-        Testa igen eller gå tillbaka till startsidan.
-      </p>
-    </div>
-    <div class="row justify-content-center">
-      <a href="/"><button class="aoh-btn-solid">Gå till startsida</button></a>
+    <div class="container d-flex flex-column justify-content-center h-100">
+      <div class="row justify-content-center p-5">
+        <?php 
+          $img = wp_get_attachment_image( 9, 'medium' );
+          echo $img;
+        ?>
+      </div>
+      <div class="row m-auto container-sm">
+        <p class="text-center">Hoppsan hejsan! Vi kunde inte hitta sidan du letade efter.
+          Det beror förmodligen på att den inte finns.
+          Testa igen eller gå tillbaka till startsidan.
+        </p>
+      </div>
+      <div class="row justify-content-center">
+        <a href="/"><button class="aoh-btn-solid m-4">Gå till startsida</button></a>
+      </div>
     </div>
   </article>
 </section>
