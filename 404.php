1e3ad9c88b0b3531515e56a139f89f5905bf0290
David Larsson <davvelars@gmail.com>
2020-12-08 13:37:29 +0100

    css-fix

--- 404.php
+++ 404.php
@@ -7,24 +7,24 @@ get_header('small', false);
 ?>
 
 <section class="container col">
+  <h1 class="row justify-content-center border-bottom border-dark">
+    Not found
+  </h1>
   <article class="col aoh-404-content">
-    <h1 class="row justify-content-center border-bottom border-dark">
-      Not found
-    </h1>
-    <div class="container d-flex flex-column justify-content-center h-100">
-      <div class="row justify-content-center p-5">
+    <div class="aoh-404-inner">
+      <div class="justify-content-center p-5">
         <?php 
           $img = wp_get_attachment_image( 9, 'medium' );
           echo $img;
         ?>
       </div>
-      <div class="row m-auto container-sm">
+      <div>
         <p class="text-center">Hoppsan hejsan! Vi kunde inte hitta sidan du letade efter.
           Det beror förmodligen på att den inte finns.
           Testa igen eller gå tillbaka till startsidan.
         </p>
       </div>
-      <div class="row justify-content-center">
+      <div>
         <a href="/"><button class="aoh-btn-solid m-4">Gå till startsida</button></a>
       </div>
     </div>
