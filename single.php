2fd39a9376e31a5be4cb8c180e3b8cf4c0cc0cd9
iths-stefan-sulejic <54362267+iths-stefan-sulejic@users.noreply.github.com>
2020-12-08 11:21:14 +0100

    Skapat single.php

---
+++ single.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying single posts and pages.
+ *
+ * @package WordPress
+ * @subpackage Allt_Om_Havet
+ * @since Allt Om Havet 1.0
+ */
+
+get_header();
+?>
+
+    <?php
+
+	if ( have_posts() ):
+
+        while ( have_posts() ): the_post(); ?>
+        
+            <article id= "post-<?php the_ID(); ?>">
+
+                <?php the_title('<h1></h1>'); ?>
+                <?php the_category(); ?>
+                <?php the_content(); ?>
+
+            </article>
+        <?php endwhile;
+
+    endif;
+    
+	?>
+
+<?php get_footer(); ?>
