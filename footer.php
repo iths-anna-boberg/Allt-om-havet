7f815a1af15f1f73cb1035ff92850fe9b18ac7b6
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-12-08 10:21:09 +0100

    Added widget area to footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
     <footer class="d-flex flex-wrap justify-content-between aoh-footer">
       <img class="pl-4 aoh-footer-logo" src="./mockup-assets/aoh-logo.png" alt="allt om havet-logo">
-      <div></div>
+
+      <?php if (is_active_sidebar('footer')) : ?>
+        <div class="footer-widget-area">
+          <?php dynamic_sidebar('footer'); ?>
+        </div>
+      <?php endif; ?>
+
       <div class="col-lg-4 d-flex justify-content-around p-4">
         <span class="aoh-text-white">info@alltomhavet.se</span>
         <span class="aoh-text-white">0746-24 31 42</span>
