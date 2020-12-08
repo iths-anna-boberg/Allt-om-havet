93194abf60fba6be2c6e79ebfbecdc4e9b96adc7
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-12-08 09:29:43 +0100

    Added footer templating from html mockup

---
+++ footer.php
@@ -0,0 +1,10 @@
+    <footer class="d-flex flex-wrap justify-content-between aoh-footer">
+      <img class="pl-4 aoh-footer-logo" src="./mockup-assets/aoh-logo.png" alt="allt om havet-logo">
+      <div></div>
+      <div class="col-lg-4 d-flex justify-content-around p-4">
+        <span class="aoh-text-white">info@alltomhavet.se</span>
+        <span class="aoh-text-white">0746-24 31 42</span>
+      </div>
+    </footer>
+  </body>
+</html>
