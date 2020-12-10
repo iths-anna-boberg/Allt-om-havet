7f42d35fe830e3c7bd88bd3bae7e4b09ad039698
iths-jonathan-engman <54362354+iths-jonathan-engman@users.noreply.github.com>
2020-12-10 14:43:04 +0100

    first commit comments.php

--- comments.php
+++ comments.php
@@ -1,17 +1,58 @@
-<?php if($comments) : ?>
-	<ol>
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Noto_Simple
+ */
 
-  	<?php foreach($comments as $comment) : ?>
-		<li id="comment-<?php comment_ID(); ?>">
-			<?php if ($comment->comment_approved == '0') : ?>
-				<p>Din kommentar inväntar godkännande.</p>
-			<?php endif; ?>
-			<?php comment_text(); ?>
-			<cite><?php comment_type(); ?> av <?php comment_author_link(); ?> <?php comment_date(); ?> vid <?php comment_time(); ?></cite>
-		</li>
-	<?php endforeach; ?>
+if ( post_password_required() ) {
+	return;
+}
+?>
 
-	</ol>
-<?php else : ?>
-	<p>Inga kommentarer än.</p>
-<?php endif; ?>
+<div id="comments" class="comments-area">
+
+	<?php
+	if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php _e('Comments', 'noto-simple'); ?>
+		</h2>
+
+		<?php
+        the_comments_navigation(array(
+            'prev_text' =>'<i class="material-icons">navigate_before</i><span class="hidden-sm">' . __('Older comments', 'noto-simple') . '</span>',
+            'next_text' => '<span class="hidden-sm">' . __('Newer comments', 'noto-simple') . '</span><i class="material-icons">navigate_next</i>',
+        )); ?>
+
+		<ol class="comment-list">
+			<?php
+				wp_list_comments( array(
+					'style'      => 'ol',
+					'short_ping' => true,
+                    'avatar_size' => 32,
+				) );
+			?>
+		</ol>
+
+		<?php 
+        the_comments_navigation(array(
+            'prev_text' =>'<i class="material-icons">navigate_before</i><span class="hidden-sm hidden-md">' . __('Older comments', 'noto-simple') . '</span>',
+            'next_text' => '<span class="hidden-sm hidden-md">' . __('Newer comments', 'noto-simple') . '</span><i class="material-icons">navigate_next</i>',
+        ));
+
+		if ( ! comments_open() ) : ?>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'noto-simple' ); ?></p>
+		<?php
+		endif;
+
+	endif;
+
+	comment_form();
+	?>
+
+</div>
