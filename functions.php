bef7277778dbe80833f01b2ae73a658b86377e9d
iths-anna-boberg <amboberg@gmail.com>
2020-12-11 15:09:17 +0100

    add style for reviews

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
     wp_enqueue_style('aoh-fonts', 'https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', false, null, 'all');
     wp_enqueue_style('aoh-custom', get_template_directory_uri().'/style.css', array('aoh-bootstrap'), 'all');
     wp_enqueue_style('aoh-event', get_template_directory_uri().'/assets/event.css', array('aoh-bootstrap'), 'all');
+    wp_enqueue_style('aoh-reviews', get_template_directory_uri().'/assets/reviews.css', array('aoh-bootstrap'), 'all');
     wp_enqueue_script('aoh-bootstrap-js', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array('jquery'), null, true);
     wp_enqueue_script('aoh-bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true);
   }
--- template-parts/content-hamn.php
+++ template-parts/content-hamn.php
@@ -9,6 +9,7 @@
 
         <?php the_content(); ?>
     </div>
+    
     <div class="col-sm-4">
         <div class="p-4 aoh-event-box aoh-box-lightblue">
             <?php get_template_part('template-parts/content', 'faciliteter');?>
