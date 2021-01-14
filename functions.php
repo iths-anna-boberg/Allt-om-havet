4f55ae99ba36021419f967759f5f8e53c615b8d4
David Larsson <davvelars@gmail.com>
2021-01-14 15:38:40 +0100

    Arcive-hamn så gott som klar, ej responsiv

--- functions.php
+++ functions.php
@@ -111,4 +111,16 @@
 
   add_action('init', 'aoh_create_post_type_dock');
 
+
+
+  function template_chooser( $template ){
+    global $wp_query;   
+    $post_type = get_query_var('post_type');   
+    if( $wp_query->is_search && $post_type == 'hamn' ){
+        return locate_template('archive-hamn.php');  //  redirect to archive-search.php
+    }   
+    return $template;   
+}
+add_filter( 'template_include', 'template_chooser' ); 
+
 ?>
