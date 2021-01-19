8b888dc71758e3aad79646c9efb0f3f2b3580600
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2021-01-19 10:22:09 +0100

    Added closestDockBtn to the DOM

--- scripts/closest_to_user.js
+++ scripts/closest_to_user.js
@@ -1,12 +1,15 @@
-if(navigator.geolocation){
+if(navigator.geolocation){ // If the users browser supports and the user accepts geolocation
   navigator.geolocation.getCurrentPosition(async (pos) => {
+    // Get the users position
     let lat = pos.coords.latitude; 
     let lon = pos.coords.longitude;
 
+    // Get all docks with their respective coords from our custom endpoint in WP
     let dockList = await fetch('/wp-json/alltomhavet/v1/get_dock_coords');
     dockList = await dockList.text();
     dockList = JSON.parse(dockList);
 
+    // Make a comparable list of distances per dock
     dockList = dockList
       .map(dock => {
         let distance = Math.abs(
@@ -23,9 +26,16 @@ if(navigator.geolocation){
       })
       .sort((a,b) => a.distance - b.distance);
 
-
-    let closestDock = dockList[0];
-
-    // TODO - Make a button/link with closestDock and place it in the header
+    let closestDockObj = dockList[0];
+    
+    // Add it to the DOM
+    let navbar = document.querySelector('.navbar');
+    let closestDockBtn = document.createElement('button');
+    closestDockBtn.innerHTML = `Gå till ${closestDockObj.name}, din närmsta hamn`; 
+    closestDockBtn.classList.add("aoh-btn-solid","aoh-max-width-250px");
+    navbar.appendChild(closestDockBtn);
+    closestDockBtn.addEventListener('click', e => {
+      window.location.href = closestDockObj.permalink;
+    })
   })
 }
