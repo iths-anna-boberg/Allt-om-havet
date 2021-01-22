7188cc24bfc44d2e4ca96d3abf6b7fcf79d76a7a
iths-anna-boberg <amboberg@gmail.com>
2021-01-22 10:13:22 +0100

    fix indentation

--- template-parts/content-best_rated.php
+++ template-parts/content-best_rated.php
@@ -28,10 +28,10 @@
           }
       
           array_multisort(array_column($posts, 'rating'), SORT_DESC, $posts);
+          
           $three_posts = array_slice($posts, 0, 3);
-      
 
-        foreach ($three_posts as $rating_card) {
+          foreach ($three_posts as $rating_card) {
             echo '  <div class="col d-flex flex-column align-items-center">
                         <img class="aoh-thumbnail-hamn" src=' . $rating_card["img_link"] .'  alt=' . $rating_card["post_title"] . ' >
                         <h4 class="mt-2"> ' . $rating_card["post_title"] . '</h6>
@@ -41,6 +41,6 @@
                         <a class="mb-5 aoh-btn-solid aoh-link-btn" href=' . $rating_card["post_link"] .'>' . $rating_card["post_title"] .'</a>
                         
                     </div>  ';
-        }
+          }
       ?>
       </article>
