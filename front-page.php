d09af6c4c091558fb8c0903ede5f4e097bc5d724
iths-anna-boberg <amboberg@gmail.com>
2021-01-22 14:06:57 +0100

    Feature/front page (#36)
    * test query reviews
    
    * add best rated hamnar to frontpage
    
    * add best rated hamn frontpage
    
    * add cursor pointer on btn
    
    * fix three posts best rated
    
    * fix indentation
    
    * small fix to variable name
    
    * add carousel to front-page

--- front-page.php
+++ front-page.php
@@ -28,13 +28,19 @@
     </div>
 
     <div class="mt-5">
+
+      <?php get_template_part('template-parts/content', 'carousel');?>
+
+    </div>
+
+    <div class="mt-5">
       <article class="col-sm-12">
         <h3 class="mb-4">Populäraste hamnarna 2020</h3>
       </article>
 
       <?php get_template_part('template-parts/content', 'best_rated');?>
 
-      </div>
+    </div>
   </section>
 
   <?php
