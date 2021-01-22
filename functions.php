294805dc8499eb6baa5f68238adf500bdd07e53c
David Larsson <davvelars@gmail.com>
2021-01-22 09:32:47 +0100

    fix for the nav

--- functions.php
+++ functions.php
@@ -1,15 +1,17 @@
 <?php
 
   function aoh_enqueue_scripts(){
-    wp_enqueue_style('aoh-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css', false, null, 'all');
+    wp_enqueue_style('aoh-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', false, null, 'all');
     wp_enqueue_style('aoh-fonts', 'https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', false, null, 'all');
     wp_enqueue_style('aoh-custom', get_template_directory_uri().'/style.css', array('aoh-bootstrap'), 'all');
     wp_enqueue_style('aoh-event', get_template_directory_uri().'/assets/event.css', array('aoh-bootstrap'), 'all');
     wp_enqueue_style('aoh-archive-hamn', get_template_directory_uri().'/assets/archive-hamn.css', array('aoh-bootstrap'), 'all');
     wp_enqueue_style('aoh-reviews', get_template_directory_uri().'/assets/reviews.css', array('aoh-bootstrap'), 'all');
-    wp_enqueue_script('aoh-bootstrap-js', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array('jquery'), null, true);
-    wp_enqueue_script('aoh-bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true);
+    wp_enqueue_script('aoh-bootstrap-jq', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', array('jquery'), null, true);
+    wp_enqueue_script('aoh-bootstrap-popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array('jquery'), null, true);
+    wp_enqueue_script('aoh-bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array('jquery'), null, true);
   }
+
 
   add_action('wp_enqueue_scripts', 'aoh_enqueue_scripts');
 
