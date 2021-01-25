b3aa35ffe536fba1286c4a82057b8794f835c348
iths-linus-kallin <jaglinus@gmail.com>
2021-01-25 14:12:53 +0100

    sticky footer på hamn-archive och evenemang

--- header.php
+++ header.php
@@ -35,9 +35,10 @@
     </nav>
     <div class="jumbotron aoh-jumbotron">
       <div class="container aoh-hero-cta">
-        <h1 class="aoh-hero-heading">Sover du också på lektionstid?</h1>
-        <p class="aoh-hero-preamble">Boka en hamnplats hos oss istället.</p>
-        <a class="aoh-btn-solid" href="./hamnar">Boka hamnplats</a>
+        <h1 class="aoh-hero-heading">Välkommen till Allt om havet!</h1>
+        <p class="aoh-hero-preamble">Här finns hela Sveriges gästhamnar</p>
+        <p><span class="aoh-nearest-dock"></span></p>
+        <a class="aoh-btn-solid" href="./hamnar">Boka hamnplats <span class="aoh-nearest-dock-btn"></span></a>
       </div>
     </div>
   </header>
